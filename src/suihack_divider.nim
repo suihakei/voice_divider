d096d66dc3ea5c0c066f5c2ce13cb393c273f201
水波形 <suihakei@gmail.com>
2021-08-02 00:07:12 +0900

    前後の無音をカットする機能追加

--- src/suihack_divider.nim
+++ src/suihack_divider.nim
@@ -3,7 +3,7 @@ import wav
 import strutils
 import strformat
 import locks
-import wNim/[wApp, wFrame, wPanel, wStaticText, wTextCtrl, wButton, wFileDialog, wDirDialog, wMessageDialog]
+import wNim/[wApp, wFrame, wPanel, wStaticText, wTextCtrl, wButton, wFileDialog, wDirDialog, wMessageDialog, wCheckBox]
 
 
 ##
@@ -21,7 +21,13 @@ const STATUS_DEFAULT_MESSAGE = "やってることがここに表示されます
 ##
 ## 前方宣言
 ##
-proc divideWav(data: tuple[targetWavFilePath: string, outputDirPath: string, threshold: float, silenceTimeToCut: float]) {.thread.}
+proc divideWav(data: tuple[targetWavFilePath: string, outputDirPath: string, threshold: float, silenceTimeToCut: float, trimSlienceTime: float]) {.thread.}
+
+
+##
+## グローバル変数
+##
+var thread: Thread[tuple[targetWavFilePath: string, outputDirPath: string, threshold: float, silenceTimeToCut: float, trimSlienceTime: float]]
 
 
 # GUI設定
@@ -52,9 +58,13 @@ let textThresholdVolume = TextCtrl(panel, pos=(110, 100), size=(80, 19), style=w
 let labelSilenceTimeToCut = StaticText(panel, label="カットポジション判別秒数", pos=(10, 125), size=(130, 21))
 let textSilenceTimeToCut = TextCtrl(panel, pos=(145, 125), size=(80, 19), style=wBorderSimple)
 
-let labelOutputFolder = StaticText(panel, label="出力先ディレクトリ", pos=(10, 170), size=(150, 21))
-let textOutputFolder = TextCtrl(panel, pos=(10, 190), size=(550, 19), style=wBorderSimple)
-let buttonOutputFolderSelect = Button(panel, pos=(570, 170), size=(50, 40), label="選択")
+let checkBoxTrimSilence = CheckBox(panel, label="前後の無音のカット", pos=(10, 145))
+let labelTrimSilence = StaticText(panel, label="前後の無音を残す秒数", pos=(145, 150), size=(130, 21))
+let textTrimSilence = TextCtrl(panel, pos=(280, 148), size=(80, 19), style=wBorderSimple)
+
+let labelOutputFolder = StaticText(panel, label="出力先ディレクトリ", pos=(10, 180), size=(150, 21))
+let textOutputFolder = TextCtrl(panel, pos=(10, 200), size=(550, 19), style=wBorderSimple)
+let buttonOutputFolderSelect = Button(panel, pos=(570, 180), size=(50, 40), label="選択")
 
 let buttonStartDivide = Button(panel, pos=(470, 240), size=(150, 40), label="無音でカット開始")
 let textStatus = TextCtrl(panel, value=STATUS_DEFAULT_MESSAGE, pos=(10, 250), size=(450, 21), style=wAlignCentre or wTeReadOnly)
@@ -65,6 +75,7 @@ let textStatus = TextCtrl(panel, value=STATUS_DEFAULT_MESSAGE, pos=(10, 250), si
 #
 textThresholdVolume.setValue("0.05")
 textSilenceTimeToCut.setValue("2")
+textTrimSilence.setValue("0.5")
 
 
 #
@@ -92,12 +103,13 @@ buttonOutputFolderSelect.wEvent_Button do (event: wEvent):
 #
 # 無音でカット開始
 #
-var thread: Thread[tuple[targetWavFilePath: string, outputDirPath: string, threshold: float, silenceTimeToCut: float]]
 buttonStartDivide.wEvent_Button do (event: wEvent):
     let wavFile = textCutTargetFile.getValue()
     let dir = textOutputFolder.getValue()
     let threshold = parseFloat(textThresholdVolume.getValue())
     let silenceTimeToCut = parseFloat(textSilenceTimeToCut.getValue())
+    let isTrimSilence = checkBoxTrimSilence.isChecked()
+    var trimSlienceTime = parseFloat(textTrimSilence.getValue())
 
     if wavFile == "":
         MessageDialog(frame, "WAVファイルが指定されていません").display()
@@ -119,7 +131,14 @@ buttonStartDivide.wEvent_Button do (event: wEvent):
         MessageDialog(frame, "カットポジション判別秒数は0.1以上で指定してください").display()
         return
 
-    createThread(thread, divideWav, (wavFile, dir, threshold, silenceTimeToCut))
+    if isTrimSilence == false:
+        trimSlienceTime = 0.0
+
+    if isTrimSilence == true and trimSlienceTime < 0:
+        MessageDialog(frame, "前後の無音のカット秒数は0以上で指定してください").display()
+        return
+
+    createThread(thread, divideWav, (wavFile, dir, threshold, silenceTimeToCut, trimSlienceTime))
 
 
 frame.center()
@@ -127,7 +146,7 @@ frame.show()
 app.mainLoop()
 
 
-proc divideWav(data: tuple[targetWavFilePath: string, outputDirPath: string, threshold: float, silenceTimeToCut: float]) {.thread.} =
+proc divideWav(data: tuple[targetWavFilePath: string, outputDirPath: string, threshold: float, silenceTimeToCut: float, trimSlienceTime: float]) {.thread.} =
     {.gcsafe.}:
         textStatus.setValue("WAVファイルの分割中...")
 
@@ -151,7 +170,8 @@ proc divideWav(data: tuple[targetWavFilePath: string, outputDirPath: string, thr
                 continue
 
             # 前後の無音をカット
-            slicedWave = wav.trimSilence(slicedWave, data.threshold, data.silenceTimeToCut, 0.8)
+            if data.trimSlienceTime > 0:
+                slicedWave = wav.trimSilence(slicedWave, data.threshold, data.silenceTimeToCut, data.trimSlienceTime)
 
             # 書き出し
             wav.writeWave(data.outputDirPath / $outputCnt & ".wav", slicedWave)
